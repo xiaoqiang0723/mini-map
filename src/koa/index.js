c621a40a62122f0011b6dafeda935a9519d80cc1
xiaoqiang0723 <457954719@qq.com>
2020-04-13 15:44:14 +0800

    修改https方式

--- src/koa/index.js
+++ src/koa/index.js
@@ -4,18 +4,11 @@ const koaqs = require('koa-qs')
 const Router = require('koa-router')
 const body = require('koa-body')
 const compress = require('koa-compress')
-const https = require('https')
-const fs = require('fs')
 
 const config = require('../../config.js')
 
 const app = new Koa()
 const router = new Router()
-
-const options = {
-	key: fs.readFileSync('./src/cert/woyezhi.key'),
-	cert: fs.readFileSync('./src/cert/woyezhi.pem'),
-}
 
 app.use(compress())
 app.use(body({ multipart: true, parsedMethods: ['post', 'put', 'get', 'delete'], formidable: { maxFileSize: 400 * 1024 * 1024, maxFields: 10, maxFieldsSize: 4000 * 1024 * 1024 } }))
@@ -26,7 +19,6 @@ function enableRouter() {
 	app.use(router.allowedMethods())
 }
 
-
 const DEFAULT_CONFIG = {
 	port: 443,
 	host: '0.0.0.0',
@@ -36,7 +28,7 @@ const conf = _.extend(DEFAULT_CONFIG, config.web)
 const port = process.env.port || conf.port
 const host = process.env.host || conf.host
 
-https.createServer(options, app.callback()).listen(port, host, () => {
+app.listen(port, host, () => {
 	console.log('Start Http Server @ %s:%s', host, port)
 })
 
